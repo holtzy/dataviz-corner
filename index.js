23dca48ec05fef6cf1c56d7ade4481c6d2922059
holtzy <yan.holtz@datadoghq.com>
2023-02-13 23:51:21 +0100

    fetch first rss feed

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 // Import the filesystem module
 const fs = require('fs');
-// let Parser = require('rss-parser');
-// let parser = new Parser();
+let Parser = require('rss-parser');
+let parser = new Parser();
 
-// (async () => {
+(async () => {
 
-//   let feed = await parser.parseURL('https://www.reddit.com/.rss');
-//   console.log(feed.title);
+  let feed = await parser.parseURL('https://www.reddit.com/.rss');
+  console.log(feed.title);
 
-//   feed.items.forEach(item => {
-//     console.log(item.title + ':' + item.link)
-//   });
+  feed.items.forEach(item => {
+    console.log(item.title + ':' + item.link)
+  });
 
-// })();
+})();
 
 const obj = {
     toto: 2,
