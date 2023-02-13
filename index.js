bc89093816e119dccf234bdf5616b2d50d3bc669
holtzy <yan.holtz@datadoghq.com>
2023-02-14 00:02:27 +0100

    use ncc to be able to use dependency in action

--- index.js
+++ index.js
@@ -9,28 +9,27 @@ let parser = new Parser();
   console.log(feed.title);
 
   feed.items.forEach(item => {
-    console.log(item.title + ':' + item.link)
+    console.log(item.link)
   });
+
+  var json = JSON.stringify(feed);
+
+    fs.writeFile("./data.json", json,
+    {
+        encoding: "utf8",
+        flag: "w",
+        mode: 0o666
+    },
+    (err) => {
+        if (err)
+        console.log(err);
+        else {
+        console.log("File written successfully\n");
+        }
+    });
+
 
 })();
 
-const obj = {
-    toto: 2,
-    date: new Date()
-}
 
-var json = JSON.stringify(obj);
 
-fs.writeFile("./data.json", json,
-  {
-    encoding: "utf8",
-    flag: "w",
-    mode: 0o666
-  },
-  (err) => {
-    if (err)
-      console.log(err);
-    else {
-      console.log("File written successfully\n");
-    }
-});
