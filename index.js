b7eaa67645909c1619fda34c9d4a6306889df6e4
holtzy <yan.holtz@datadoghq.com>
2023-02-13 22:34:24 +0100

    test write JSON

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('Yan says hello world')
+// Import the filesystem module
+const fs = require('fs');
+
+const obj = {
+    toto: 2
+}
+
+var json = JSON.stringify(obj);
+
+fs.writeFile("./data.json", json,
+  {
+    encoding: "utf8",
+    flag: "w",
+    mode: 0o666
+  },
+  (err) => {
+    if (err)
+      console.log(err);
+    else {
+      console.log("File written successfully\n");
+    }
+});
