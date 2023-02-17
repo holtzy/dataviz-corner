42219aa6476d94f98486d68a9bb29225d79a2fcc
holtzy <yan.holtz@datadoghq.com>
2023-02-17 17:35:32 +0100

    format js fetching file

--- index.js
+++ index.js
@@ -1,38 +1,40 @@
-// Import the filesystem module
-const fs = require('fs');
-
-// List of blogs to fetch stored in an array
 import { blogs } from 'data/blogs.ts'
 
-let Parser = require('rss-parser');
-let parser = new Parser();
+// This is the script run to harvest rss feeds every x hours.
+// To be run by the github action, it needs to be bundled with the necessary library
+// This is possible thanks to the ncc lib using:
+
+// ncc build index.js -o dist
+
+// Do it each time this file is modified
+
+// Import the filesystem module
+const fs = require('fs')
+
+// List of blogs to fetch stored in an array
+
+const Parser = require('rss-parser')
+const parser = new Parser()
 
 Promise.all(blogs.map(blog => parser.parseURL(blog.feedUrl)))
-    .then(res => {
-        console.log('------')
-        console.log(res.map(blog => blog.feedUrl))
+  .then(res => {
+    console.log('------')
+    console.log(res.map(blog => blog.feedUrl))
 
-        const cleanRes = res.flatMap(blog => blog.items)
+    const cleanRes = res.flatMap(blog => blog.items)
 
-        var json = JSON.stringify(cleanRes);
+    const json = JSON.stringify(cleanRes)
 
-        fs.writeFile("./data/data.json", json,
-        {
-            encoding: "utf8",
-            flag: "w",
-            mode: 0o666
-        },
-        (err) => {
-            if (err)
-            console.log(err);
-            else {
-            console.log("File   written  successfully\n");
-            console.log(cleanRes.length + ' blogPost harvested')
-            }
-        });
-    })
-
-
-
-
-
+    fs.writeFile('./data/data.json', json,
+      {
+        encoding: 'utf8',
+        flag: 'w',
+        mode: 0o666
+      },
+      (err) => {
+        if (err) { console.log(err) } else {
+          console.log('File   written  successfully\n')
+          console.log(cleanRes.length + ' blogPost harvested')
+        }
+      })
+  })
