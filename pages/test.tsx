50948b4acf41d9a77061f9878849ae52e47d7f92
holtzy <yan.holtz@datadoghq.com>
2023-02-21 14:38:02 +0100

    test with circles

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,57 +1,82 @@
+// Video to understand perlin Noise: https://www.youtube.com/watch?v=ikwNrFvnL3g
+
 import * as d3 from "d3";
 import { useCallback, useEffect, useRef, useState } from "react";
 import Perlin from "Utils/perlin";
 
-const offset = 0;
-const noiseFactor = 0.05;
-
+//
+// Create initial dataset
+// 300 evenly-spaced points
+//
 const width = 600;
 const height = 500;
+const gapSize = 30; // space in px between 2 data points
+
+type Data = {
+  x: number; // used to translate the shape later on
+  xVal: number;
+  y: number;
+  yVal: number;
+}[];
+
+const getData = (): Data => {
+  const xCount = Math.floor(width / gapSize);
+  const yCount = Math.floor(height / gapSize);
+  const numPoints = xCount * yCount;
+
+  const data = d3.range(numPoints).map((i) => {
+    const xVal = i % xCount,
+      x = xVal * gapSize,
+      yVal = Math.floor(i / xCount),
+      y = yVal * gapSize;
+    return { x, xVal, y, yVal };
+  });
+
+  return data;
+};
+
+//
+// A component that initialize the dataset and render the graph.
+// Then create a loop to update the perlin offset progressively.
+//
+const OFFSET_STEP = 0.008; // if offset_step is higher, we move each time to a step that is further in the perlin noise, thus more different. It results in an animation that looks faster.
 
 export default function Home() {
   const data = getData();
 
   const [offset, setOffset] = useState(0);
 
-  console.log(" ----", offset);
   useEffect(() => {
     setTimeout(() => {
-      setOffset(offset + 0.01);
-    }, 100);
+      setOffset(offset + OFFSET_STEP);
+    }, 50);
   }, [offset]);
 
-  // while (true) {
-  //   setTimeout(() => {
-  //     console.log(offset);
-  //     //   setOffset(offset + 0.01);
-  //   }),
-  //     1000;
-  // }
-  // for (let i = 0; i < 100; i++) {
-  //   setTimeout(() => {
-  //     console.log(i);
-  //   }, 10000);
-  // }
-
   return (
-    <div onClick={() => setOffset(offset + 0.1)}>
+    <div>
       <Graph offset={offset} data={data} />
     </div>
   );
 }
 
-// renderer
+//
+// Initialize a perlin noise calculator
+// perlin.perlin2(0.1, 0.3) --> 0.1648953425443803
+//
+const perlin = new Perlin(Math.random());
+
+const noiseFactor = 0.08;
+
+//
+// renderer: just draw the final output based on the perline Noise Value and an offset
+//
 type GraphProps = {
   offset: number;
-  data: {
-    x: number;
-    xVal: number;
-    y: number;
-    yVal: number;
-  }[];
+  data: Data;
 };
+
 const Graph = ({ offset, data }: GraphProps) => {
-  const color = d3.interpolateRainbow;
+  const color = d3.interpolateViridis;
 
   const allLines = data.map((item, i) => {
     return (
@@ -61,24 +86,33 @@ const Graph = ({ offset, data }: GraphProps) => {
           transform: `translate(${item.x}px,${item.y}px)`,
         }}
       >
-        <line
+        <circle
+          cx={0}
+          cy={0}
+          r={
+            (gapSize / 2) *
+            Math.cos(
+              2 *
+                Math.PI *
+                perlin.perlin2(item.xVal * noiseFactor + offset, item.yVal * noiseFactor + offset)
+            )
+          }
+          fill={color(
+            perlin.perlin2(item.xVal * noiseFactor + offset, item.yVal * noiseFactor + offset)
+          )}
+        ></circle>
+        {/* <line
           x1={0}
           y1={0}
           stroke={color(
-            n.perlin2(
-              item.xVal * noiseFactor + offset,
-              item.yVal * noiseFactor + offset
-            )
+            perlin.perlin2(item.xVal * noiseFactor + offset, item.yVal * noiseFactor + offset)
           )}
           x2={
             gapSize *
             Math.sin(
               2 *
                 Math.PI *
-                n.perlin2(
-                  item.xVal * noiseFactor + offset,
-                  item.yVal * noiseFactor + offset
-                )
+                perlin.perlin2(item.xVal * noiseFactor + offset, item.yVal * noiseFactor + offset)
             )
           }
           y2={
@@ -86,14 +120,11 @@ const Graph = ({ offset, data }: GraphProps) => {
             Math.cos(
               2 *
                 Math.PI *
-                n.perlin2(
-                  item.xVal * noiseFactor + offset,
-                  item.yVal * noiseFactor + offset
-                )
+                perlin.perlin2(item.xVal * noiseFactor + offset, item.yVal * noiseFactor + offset)
             )
           }
           strokeWidth={2}
-        ></line>
+        ></line> */}
       </g>
     );
   });
@@ -105,29 +136,4 @@ const Graph = ({ offset, data }: GraphProps) => {
       </svg>
     </div>
   );
-};
-
-// Create initial dataset
-// 300 evenly-spaced points
-const gapSize = 30;
-const n = new Perlin(Math.random());
-
-const getData = () => {
-  const noiseFactor = 0.4;
-
-  const xCount = Math.floor(width / gapSize),
-    yCount = Math.floor(height / gapSize),
-    numPoints = xCount * yCount;
-
-  const d = d3.range(numPoints).map((i) => {
-    const xVal = i % xCount,
-      x = xVal * gapSize,
-      yVal = Math.floor(i / xCount),
-      y = yVal * gapSize,
-      value = n.perlin2(x / (width * noiseFactor), y / (height * noiseFactor));
-
-    return { x, xVal, y, yVal };
-  });
-
-  return d;
 };
