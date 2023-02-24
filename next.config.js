da02aaf03bb54a06bc76e6e95bae6626f2ebcd46
holtzy <yan.holtz@datadoghq.com>
2023-02-24 19:04:45 +0100

    who knows

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
     // your project has type errors.
     // !! WARN !!
-    ignoreBuildErrors: true,
-  },
+    ignoreBuildErrors: true
+  }
 }
-
-module.exports = nextConfig
